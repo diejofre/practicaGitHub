2f6da950ef51bd6c5e0c3bcaf315a06359edfda1
Diego <diego-jofre@live.com.ar>
2022-09-09 10:34:16 -0400

    agregué la ruta de personas

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+const personas = [
+  {
+    nombre: "Pepe",
+    edad: 28,
+  },
+  {
+    nombre: "José",
+    edad: 45,
+  },
+];
+
 app.get("/", function (req, res) {
   res.send("Hello World");
+});
+
+app.get("/personas", (req, res) => {
+  res.send(personas);
 });
 
 app.listen(3000);
